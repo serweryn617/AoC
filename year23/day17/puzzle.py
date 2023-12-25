7299d766145967c56595c0a1717ba2db9141568d
serweryn617 <seweryn.r.1999@gmail.com>
2023-12-25 17:36:56 +0100

    Finish day 17 part 2

--- year23/day17/puzzle.py
+++ year23/day17/puzzle.py
@@ -1,68 +1,36 @@
-STEPS = (-3, -2, -1, 1, 2, 3)
-
-
-class Head:
-    def __init__(self, position, length = 0, visited = ()):
-        self.pos = position
-        self.length = length
-        self.visited = visited
-
-    def sort_key(self):
-        return self.length
-
-
-def step(head, grid, size = 3):
-    x, y, step_vertical = head.pos
+def step(head, grid, limits):
+    heat, x, y, step_vertical = head
 
     x_min, x_max = 0, len(grid[0]) - 1
     y_min, y_max = 0, len(grid) - 1
 
     next_heads = []
 
+    # TODO: cleanup
     if step_vertical:
-        for delta in range(-size, size + 1):
-            if delta == 0 or not y_min <= y + delta <= y_max:
+        for delta in range(-limits[1], limits[1] + 1):
+            if abs(delta) < limits[0] or not y_min <= y + delta <= y_max:
                 continue
 
-            visited = list((x, vy) for vy in range(min(y, y + delta), max(y, y + delta) + 1))
+            visited = [(x, vy) for vy in range(min(y, y + delta), max(y, y + delta) + 1)]
             visited.pop(visited.index((x, y)))
             weight = sum([grid[vy][vx] for vx, vy in visited])
 
-            new_head = Head((x, y + delta, not step_vertical), head.length + weight, head.visited + tuple(visited))
+            new_head = heat + weight, x, y + delta, not step_vertical
             next_heads.append(new_head)
     else:  # step horizontal
-        for delta in range(-size, size + 1):
-            if delta == 0 or not x_min <= x + delta <= x_max:
+        for delta in range(-limits[1], limits[1] + 1):
+            if abs(delta) < limits[0] or not x_min <= x + delta <= x_max:
                 continue
 
-            visited = list((vx, y) for vx in range(min(x, x + delta), max(x, x + delta) + 1))
+            visited = [(vx, y) for vx in range(min(x, x + delta), max(x, x + delta) + 1)]
             visited.pop(visited.index((x, y)))
             weight = sum([grid[vy][vx] for vx, vy in visited])
 
-            new_head = Head((x + delta, y, not step_vertical), head.length + weight, head.visited + tuple(visited))
+            new_head = heat + weight, x + delta, y, not step_vertical
             next_heads.append(new_head)
 
-    # print(f'Next for {head.pos}: {head.length}')
-    # for h in next_heads:
-    #     print(h.pos, h.length)
-
     return next_heads
-
-
-def display_visited(visited):
-    print('Visited:')
-    for v in visited:
-        print(v, end=' ')
-    print()
-
-    print('Graph:')
-    for y in range(13):
-        for x in range(13):
-            if (x, y) in visited:
-                print('X', end='')
-            else:
-                print('.', end='')
-        print()
 
 
 def loader(input_path):
@@ -76,64 +44,45 @@ def loader(input_path):
 
 
 def solver(input_path, puzzle_type):
-    # assert puzzle_type in ('energized', 'max')
+    assert puzzle_type in ('heat', 'ultra')
+
+    limits = {'heat': (1, 3), 'ultra': (4, 10)}[puzzle_type]
 
     grid = loader(input_path)
 
-    # Z true - next step vertical
+    # TODO: use class to create objects
     heads = [
-        Head((0, 0, True), visited=((0,0),)),
-        Head((0, 0, False), visited=((0,0),)),
+        # heat, x, y, next_vertical
+        (0, 0, 0, True),
+        (0, 0, 0, False),
     ]
-    # heads = [
-    #     Head((1, 0, True), grid[0][1], ((0, 0), (1, 0))),
-    #     Head((2, 0, True), grid[0][1] + grid[0][2], ((0, 0), (1, 0), (2, 0))),
-    #     Head((0, 1, False), grid[1][0], ((0, 0), (0, 1))),
-    #     Head((0, 2, False), grid[1][0] + grid[2][0], ((0, 0), (0, 1), (0, 2))),
-    # ]
+    visited = set()
+
     end = len(grid[0]) - 1, len(grid) - 1
 
     while True:
-        # head = min(heads, key=Head.sort_key)
-        heads.sort(key=Head.sort_key)
-        head = heads[0]
+        heads.sort()
+        heat, x, y, next_vertical = heads.pop(0)
 
-        # print('selecting', heads[0].pos, heads[0].length)
+        if (x, y) == end:
+            return heat
 
-        if head.pos[:2] == end:
-            print('end', head.length)
-            display_visited(head.visited)
-            return head.length
-            break
+        if (x, y, next_vertical) in visited:
+            continue
 
-        new_heads = step(head, grid)
-        heads_pos = [h.pos for h in heads]
+        visited.add((x, y, next_vertical))
+        new_heads = step((heat, x, y, next_vertical), grid, limits)
 
-        for new in new_heads:
-            if new.pos not in heads_pos:
-                # print('appending', new.pos, new.length)
-                heads.append(new)
-            else:
-                idx = heads_pos.index(new.pos)
-                if new.length < heads[idx].length:
-                    heads.pop(idx)
-                    heads.append(new)
-
-        heads.pop(heads.index(head))
-
-        print('m', min(h.length for h in heads), len(heads))
-        # print('m', len(set(h.pos for h in heads)), len(heads))
-
-    # for h in heads:
-    #     print('Pos list', h.pos)
-
-    return min_heat_loss
+        for new_head in new_heads:
+            if new_head not in visited:
+                heads.append(new_head)
 
 
 def run_examples():
     examples = (
-        ('test_input', 'heat', 102),
-        # ('test_input', 'max', 51),
+        ('test_input1', 'heat', 102),
+        ('test_input1', 'ultra', 94),
+        ('test_input2', 'ultra', 71),
     )
 
     for path, puzzle_type, expected in examples:
@@ -147,23 +96,20 @@ def main():
     import time
     start_time = time.time()
 
-    part1 = solver('input', 'energized')
-    # part2 = solver('input', 'max')
+    part1 = solver('input', 'heat')
+    part2 = solver('input', 'ultra')
 
     took = time.time() - start_time
 
-    print('Puzzle 1 answer:', part1)
-    # print('Puzzle 2 answer:', part2)
-    print(f'Both solutions found in {took:.3f}s')  # 735ms
+    # print('Puzzle 1 answer:', part1)
+    print('Puzzle 2 answer:', part2)
+    print(f'Both solutions found in {took:.3f}s')  # 275000ms
 
     # Regression test
-    # assert part1 == 
-    # assert part2 == 
+    assert part1 == 638
+    assert part2 == 748
 
 
 if __name__ == '__main__':
     run_examples()
     main()
-
-    # import cProfile
-    # cProfile.run("solver('test_input', 1)")
