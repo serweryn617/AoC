beb48f5942ed5d5ada48fd3d18f3c5fe853b2871
serweryn617 <seweryn.r.1999@gmail.com>
2025-12-07 19:59:23 +0100

    update template

--- template.py
+++ template.py
@@ -1,26 +1,28 @@
-def solve_part1(parsed_input, is_example):
+def solve_part1(parsed_input):
     return 0
 
 
-def solve_part2(parsed_input, is_example):
+def solve_part2(parsed_input):
     return 0
 
 
 def loader(input_path):
+    data = []
+
     with open(input_path, 'r') as puzzle:
-        for line in puzzle.readlines():
-            pass
+        for line in puzzle:
+            data.append(line.strip())
 
-    return None
+    return data
 
 
-def solver(input_path, part, is_example=False):
+def solver(input_path, part):
     parsed_input = loader(input_path)
 
     if part == 1:
-        result = solve_part1(parsed_input, is_example)
+        result = solve_part1(parsed_input)
     else:
-        result = solve_part2(parsed_input, is_example)
+        result = solve_part2(parsed_input)
 
     return result
 
@@ -28,11 +30,11 @@ def solver(input_path, part, is_example=False):
 def run_examples():
     examples = (
         ('test_input', 1, 0),
-        ('test_input', 2, 0),
+        # ('test_input', 2, 0),
     )
 
     for path, puzzle_type, expected in examples:
-        result = solver(path, puzzle_type, is_example=True)
+        result = solver(path, puzzle_type)
         assert result == expected, f'Example {path} {puzzle_type} failed: {result}'
 
     print("Examples passed")
@@ -43,13 +45,13 @@ def main():
     start_time = time.time()
 
     part1 = solver('input', 1)
-    part2 = solver('input', 2)
+    # part2 = solver('input', 2)
 
     took = time.time() - start_time
 
     print('Puzzle 1 answer:', part1)
-    print('Puzzle 2 answer:', part2)
-    print(f'Solutions found in {took:.3f}s')  # 0ms
+    # print('Puzzle 2 answer:', part2)
+    print(f'Solutions found in {took:.3f}s')  # xms
 
     # Regression test
     # assert part1 == 0
